1a9ba6cc6b645800269d5c6da8a19d46fa360ddc
karla98 <riosdiazkm@gmail.com>
2020-04-22 23:40:53 -0500

    app authentication

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'lA6gnfTV7p',
+        password: 'zOZR913oTR',
+        database: 'lA6gnfTV7p'
+    }
+
+};
